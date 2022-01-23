f04d8f4283810ac17d7d87a7db1bc6719642afcb
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-01-24 00:57:58 +0600

    Add files via upload

---
+++ VaultCracker/README.md
@@ -0,0 +1,14 @@
+# Vault-Cracker
+Program for cracking NQ Vault
+
+
+//
+Disclaimer:
+
+This program is for fun and educational purpose only.
+
+I never encourage to violate privacy.
+
+I will not responsible for any misuse of this code or program.
+
+//
---
+++ VaultCracker/about.c
@@ -0,0 +1,8 @@
+void typEff(char str[]);
+void about()
+{
+    typEff("\n\nHello!\nWelcome To Vault Cracker.\n\n");
+    typEff("Author: Labib Rahman\nWeb: https://github.com/Labib000\n\n");
+    typEff("Disclaimer:\nThis program is made for educational purpose only.\n\n");
+    typEff("No User Limitations!\nThis is an open-source and free program.\n\n");
+}
